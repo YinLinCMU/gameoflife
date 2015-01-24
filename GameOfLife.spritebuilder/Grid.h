fe4c11b6a1e4be27a1bbab7c37ba60e97ddab0fa
Yin Lin <linyin1112@gmail.com>
2015-01-24 14:32:26 -0800

    Automatically Committed for MakeGamesWithUs

---
+++ GameOfLife.spritebuilder/Grid.h
@@ -0,0 +1,13 @@
+//
+//  Grid.h
+//  GameOfLife
+//
+//  Created by Yin Lin on 1/24/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "CCSprite.h"
+
+@interface Grid : CCSprite
+
+@end
---
+++ GameOfLife.spritebuilder/Grid.m
@@ -0,0 +1,70 @@
+//
+//  Grid.m
+//  GameOfLife
+//
+//  Created by Yin Lin on 1/24/15.
+//  Copyright (c) 2015 Apportable. All rights reserved.
+//
+
+#import "Grid.h"
+#import "Creature.h"
+
+// these are variables that cannot be changed
+static const int GRID_ROWS = 8;
+static const int GRID_COLUMNS = 10;
+
+@implementation Grid{
+    NSMutableArray *_gridArray;// 2d array
+    float _cellWidth;
+    float _cellHeight;
+}
+
+- (void)onEnter
+{
+    [super onEnter];
+    
+    [self setupGrid];
+    
+    // accept touches on the grid
+    self.userInteractionEnabled = YES;
+}
+
+- (void)setupGrid
+{
+    // divide the grid's size by the number of columns/rows to figure out the right width and height of each cell
+    _cellWidth = self.contentSize.width / GRID_COLUMNS;
+    _cellHeight = self.contentSize.height / GRID_ROWS;
+    
+    float x = 0;
+    float y = 0;
+    
+    // init the array as a blank nsmutable array
+    _gridArray = [NSMutableArray array];
+    
+    //init creatures
+    for (int i = 0; i < GRID_ROWS; i++) {
+        //this is how you creat 2d arrays in oc. you put arrays into arrays
+        _gridArray[i] = [NSMutableArray array];
+        x = 0;
+        
+        for (int j = 0; j < GRID_COLUMNS; j++) {
+            Creature *creature = [[Creature alloc] initCreature];
+            creature.anchorPoint = ccp(0, 0);
+            creature.position = ccp(x, y);
+            [self addChild:creature];
+            
+            //this is shorthand to access an array inside an array
+            _gridArray[i][j] = creature;
+            
+            //make creatures visible to test this method, remove this once we know we have filled the grid properly
+            creature.isAlive = YES;
+            
+            x += _cellWidth;
+        }
+        y += _cellHeight;
+        }
+    }
+    
+
+
+@end
